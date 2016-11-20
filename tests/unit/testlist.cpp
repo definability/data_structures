5b36047ae1a395e5b4fa37b7d569120b747cc16d
char-lie <5962274+char-lie@users.noreply.github.com>
2016-11-20 17:40:50 +0200

    Testing List copy constructor

--- tests/unit/testlist.cpp
+++ tests/unit/testlist.cpp
@@ -3,3 +3,8 @@
 TYPED_TEST(ListTest, DefaultConstructorAndDestructorWorks) {
     typename TestFixture::List_ list;
 }
+
+TYPED_TEST(ListTest, CopyConstructorWorks) {
+    typename TestFixture::List_ list_a;
+    typename TestFixture::List_ list_b(list_a);
+}
