30274eb865e6ddf74d26c0cfc82fd8f3c1d823cf
Valeriy Krygin <valeriy.krygin@gmail.com>
2016-11-20 20:19:54 +0200

    Implemented unit tests for chains comparison

--- tests/unit/testlist.cpp
+++ tests/unit/testlist.cpp
@@ -1,3 +1,5 @@
+#include <memory>
+
 #include "testlist.hpp"
 
 TYPED_TEST(ListTest, ParametrisedConstructorCreatesEqual) {
@@ -6,4 +8,17 @@ TYPED_TEST(ListTest, ParametrisedConstructorCreatesEqual) {
     List_ list_a(0);
     List_ list_b(0);
     ASSERT_TRUE(list_a == list_b);
+}
+
+TYPED_TEST(ListTest, ChainsWithEqualParametersAreEqual) {
+    typedef typename TestFixture::List_ List_;
+    typedef std::shared_ptr<const List_> ListPtr_;
+
+    ListPtr_ list_aa = ListPtr_(new List_(1));
+    List_ list_ab(2, list_aa);
+
+    ListPtr_ list_ba = ListPtr_(new List_(1));
+    List_ list_bb(2, list_ba);
+
+    ASSERT_TRUE(list_ab == list_bb);
 }
