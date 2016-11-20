862b6a479f7409e59360eb202ce7aa4b32801364
char-lie <5962274+char-lie@users.noreply.github.com>
2016-11-20 18:13:10 +0200

    Implemented unit test for parametrised List constructor

--- tests/unit/testlist.cpp
+++ tests/unit/testlist.cpp
@@ -14,3 +14,9 @@ TYPED_TEST(ListTest, CopyConstructorCreatesEqual) {
     typename TestFixture::List_ list_b(list_a);
     ASSERT_TRUE(list_a == list_b);
 }
+
+TYPED_TEST(ListTest, ParametrisedConstructorCreatesEqual) {
+    typename TestFixture::List_ list_a;
+    typename TestFixture::List_ list_b(0);
+    ASSERT_TRUE(list_a == list_b);
+}
