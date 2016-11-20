e92bd808cb5aa716156ff23dacb9e6618b341c48
char-lie <5962274+char-lie@users.noreply.github.com>
2016-11-20 19:15:01 +0200

    Removed default and copy constructor unit tests
    Not needed as list is immutable

--- tests/unit/testlist.cpp
+++ tests/unit/testlist.cpp
@@ -1,22 +1,7 @@
 #include "testlist.hpp"
 
-TYPED_TEST(ListTest, DefaultConstructorAndDestructorWorks) {
-    typename TestFixture::List_ list;
-}
-
-TYPED_TEST(ListTest, CopyConstructorWorks) {
-    typename TestFixture::List_ list_a;
-    typename TestFixture::List_ list_b(list_a);
-}
-
-TYPED_TEST(ListTest, CopyConstructorCreatesEqual) {
-    typename TestFixture::List_ list_a;
-    typename TestFixture::List_ list_b(list_a);
-    ASSERT_TRUE(list_a == list_b);
-}
-
 TYPED_TEST(ListTest, ParametrisedConstructorCreatesEqual) {
-    typename TestFixture::List_ list_a;
+    typename TestFixture::List_ list_a(0);
     typename TestFixture::List_ list_b(0);
     ASSERT_TRUE(list_a == list_b);
 }
