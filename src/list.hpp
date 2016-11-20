1e62d83a4ff1f57bce3d758c84b91b188b6dbd3f
char-lie <5962274+char-lie@users.noreply.github.com>
2016-11-20 21:54:32 +0200

    Implemented 'insert' member function for List

--- src/list.hpp
+++ src/list.hpp
@@ -9,12 +9,27 @@ template<typename T> class List : public std::enable_shared_from_this< List<T> >
         const T value;
         const ListPtr next;
         const unsigned size;
+
+        ListPtr _insert(const T& value, const unsigned position) const {
+            return position? this->next->_insert(value, position - 1)
+                           : ListPtr(new List<T>(value,
+                                                 this->shared_from_this()));
+        }
     public:
         List(const T& value, const ListPtr& next=nullptr)
                 : value(value), next(next), size(next? next->size + 1 : 1) {
         }
         List(const List<T>&) = delete;
         List(const T&, const List<T>*) = delete;
+
+        ListPtr insert(const T& value, const unsigned position=0) const {
+            if (position >= size) {
+                throw std::invalid_argument(
+                    "Position should not be greater than list size"
+                );
+            }
+            return this->_insert(value, position);
+        }
 
         bool operator!=(const List<T>& list) const {
             return false
