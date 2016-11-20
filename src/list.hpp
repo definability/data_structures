d3ad0a806217b324c40883b3348ea79a70191fd6
char-lie <5962274+char-lie@users.noreply.github.com>
2016-11-20 18:07:31 +0200

    Added value and pointer to the next node to List

--- src/list.hpp
+++ src/list.hpp
@@ -2,8 +2,13 @@
 #define LIST_HPP
 
 template<typename T> class List {
+    private:
+        T value;
+        List<T>* next;
     public:
         List() {
+            this->value = 0;
+            this->next = 0;
         }
         List(const List<T>& list) {
             *(this) = list;
