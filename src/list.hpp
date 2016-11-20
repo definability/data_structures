43d8fb50bccec92cdf70290f10e6674becbb7aef
char-lie <5962274+char-lie@users.noreply.github.com>
2016-11-20 20:16:59 +0200

    Using shared pointer for List

--- src/list.hpp
+++ src/list.hpp
@@ -1,15 +1,19 @@
 #ifndef LIST_HPP
 #define LIST_HPP
 
+#include <memory>
+
 template<typename T> class List {
+    typedef std::shared_ptr< const List<T> > ListPtr;
     private:
         T value;
-        const List<T>* next;
+        ListPtr next;
     public:
-        List(const T& value, const List<T>* next = nullptr)
+        List(const T& value, const ListPtr& next=nullptr)
                 : value(value), next(next) {
         }
         List(const List<T>&) = delete;
+        List(const T&, const List<T>*) = delete;
 
         bool operator==(const List<T>& list) const {
             return !(!this->next ^ !list.next)
@@ -17,9 +21,7 @@ template<typename T> class List {
                 && (this->next == list.next || *(this->next) == *(list.next));
         }
 
-        ~List() {
-            delete this->next;
-        }
+        ~List() {}
 };
 
 #endif
