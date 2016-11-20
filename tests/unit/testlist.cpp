522218fd6e06164ae063919d64046dd9e0ada664
char-lie <5962274+char-lie@users.noreply.github.com>
2016-11-20 18:09:01 +0200

    Implemented unit test to check equality of original list with copied list

--- tests/unit/testlist.cpp
+++ tests/unit/testlist.cpp
@@ -8,3 +8,9 @@ TYPED_TEST(ListTest, CopyConstructorWorks) {
     typename TestFixture::List_ list_a;
     typename TestFixture::List_ list_b(list_a);
 }
+
+TYPED_TEST(ListTest, CopyConstructorCreatesEqual) {
+    typename TestFixture::List_ list_a;
+    typename TestFixture::List_ list_b(list_a);
+    ASSERT_TRUE(list_a == list_b);
+}
