46c1bbeed591242c60d6a98fadb64ec5787f8d06
char-lie <5962274+char-lie@users.noreply.github.com>
2016-11-20 16:04:45 +0200

    Implemented basic unit tests for templated List constructor

---
+++ tests/unit/testlist.cpp
@@ -0,0 +1,7 @@
+#include "testlist.hpp"
+#include "list.hpp"
+
+TYPED_TEST(ListTest, DefaultConstructorAndDestructorWorks) {
+    typename TestFixture::List_ list;
+    constexpr int a = 5;
+}
---
+++ tests/unit/testlist.hpp
@@ -0,0 +1,22 @@
+#include "gtest/gtest.h"
+#include "list.hpp"
+
+template<typename T> class ListTest : public ::testing::Test {
+    public:
+        typedef List<T> List_;
+        static T shared_;
+        T value_;
+    protected:
+        ListTest() {};
+        virtual ~ListTest() {};
+        virtual void SetUp() {};
+        virtual void TearDown() {};
+};
+
+typedef ::testing::Types<
+    char, short, int, long, long long,
+    unsigned char, unsigned short, unsigned int,
+    unsigned long, unsigned long long
+> MyTypes;
+
+TYPED_TEST_CASE(ListTest, MyTypes);
