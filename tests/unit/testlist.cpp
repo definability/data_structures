3d62bbe9c2c1795101ad0b2bec5ba9570ad3cf46
char-lie <5962274+char-lie@users.noreply.github.com>
2017-11-07 22:39:16 +0200

    Avoid initializer list usage for test where it's not needed

--- tests/unit/testlist.cpp
+++ tests/unit/testlist.cpp
@@ -5,8 +5,8 @@
 TYPED_TEST(ListTest, ParametrisedConstructorCreatesEqual) {
     using List_ = typename TestFixture::List_;
 
-    List_ list_a{0};
-    List_ list_b{0};
+    List_ list_a(0);
+    List_ list_b(0);
     ASSERT_TRUE(list_a == list_b);
 }
 
